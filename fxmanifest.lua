7efe53f4d56131bc016a9ffeeea6f9ffed8398c2
Rohit88612 <86488569+Rohit88612@users.noreply.github.com>
2025-02-24 23:29:51 +0530

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+lua54 'yes'
+game 'gta5'
+
+author 'Rohit'
+description 'Standalone Mailbox Pickup and Throw Script'
+version '1.0.0'
+
+shared_scripts {
+    '@ox_lib/init.lua'
+}
+
+client_scripts {
+    'main.lua'
+}
+
+dependencies {
+    'ox_lib'
+}
