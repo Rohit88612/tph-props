5ebdb98d03fbd67566469e4e674cca9fce8a798c
Rohit88612 <86488569+Rohit88612@users.noreply.github.com>
2025-03-11 23:47:45 +0530

    for the sake of maintaining

--- client.lua
+++ client.lua
@@ -54,22 +54,11 @@ local function showPreview()
     local _, groundZ = GetGroundZFor_3dCoord(playerCoords.x, playerCoords.y, playerCoords.z, false)
     playerCoords = vector3(playerCoords.x, playerCoords.y, groundZ + previewOffset.z)
     
-    previewProp = CreateObject(propModel, playerCoords.x, playerCoords.y, playerCoords.z, false, false, false)
+    previewProp = CreateObjectNoOffset(propModel, playerCoords.x, playerCoords.y, playerCoords.z, false, false, false)
     SetEntityHeading(previewProp, previewRotation)
     FreezeEntityPosition(previewProp, true)
     SetEntityAlpha(previewProp, 150, false)
-
-    Citizen.CreateThread(function()
-        while previewProp do
-            local playerCoords = GetEntityCoords(playerPed) + GetEntityForwardVector(playerPed) * previewOffset.y
-            local _, groundZ = GetGroundZFor_3dCoord(playerCoords.x, playerCoords.y, playerCoords.z, false)
-            playerCoords = vector3(playerCoords.x, playerCoords.y, groundZ + previewOffset.z)
-            
-            SetEntityCoords(previewProp, playerCoords.x, playerCoords.y, playerCoords.z)
-            SetEntityHeading(previewProp, previewRotation)
-            Wait(0)
-        end
-    end)
+    SetEntityCollision(previewProp, false, false)
 end
 
 local function pickupProp()
@@ -78,11 +67,11 @@ local function pickupProp()
     
     if object and DoesEntityExist(object) then
         propModel = GetEntityModel(object)
+        local objNetId = NetworkGetNetworkIdFromEntity(object)
+        TriggerServerEvent("blrp:pickupProp", objNetId)
+        
         loadAnimDict("anim@heists@narcotics@trash")
         TaskPlayAnim(playerPed, "anim@heists@narcotics@trash", "idle", 8.0, -8.0, -1, 49, 0, false, false, false)
-        
-        SetEntityAsMissionEntity(object, true, true)
-        DeleteEntity(object) -- Remove the original object
         
         holdingProp = CreateObject(propModel, 0, 0, 0, true, true, true)
         AttachEntityToEntity(holdingProp, playerPed, GetPedBoneIndex(playerPed, 57005), 0.2, -0.15, -0.1, 0.0, 90.0, 0.0, true, true, false, true, 1, true)
@@ -102,19 +91,10 @@ local function placeProp()
         
         local finalCoords = GetEntityCoords(playerPed) + GetEntityForwardVector(playerPed) * previewOffset.y
         local _, groundZ = GetGroundZFor_3dCoord(finalCoords.x, finalCoords.y, finalCoords.z, false)
-        finalCoords = vector3(finalCoords.x, finalCoords.y, groundZ + 0.2)
+        finalCoords = vector3(finalCoords.x, finalCoords.y, groundZ + previewOffset.z)
         
-        -- Spawn the new prop with correct properties
-        local placedProp = CreateObject(propModel, finalCoords.x, finalCoords.y, finalCoords.z, true, true, false)
-        SetEntityHeading(placedProp, previewRotation)
-
-        -- Enable physics and gravity
-        SetEntityCollision(placedProp, true, true)
-        SetEntityDynamic(placedProp, true)
-        ActivatePhysics(placedProp)
-        SetEntityHasGravity(placedProp, true)
-
-        -- If it's a heavy prop, make sure it's treated as su
+        TriggerServerEvent("blrp:placeProp", propModel, finalCoords, previewRotation)
+        
         if previewProp then
             DeleteEntity(previewProp)
             previewProp = nil
@@ -123,33 +103,6 @@ local function placeProp()
         ClearPedTasks(playerPed)
     end
 end
-
-Citizen.CreateThread(function()
-    while true do
-        if previewProp then
-            -- Arrow key movement for preview prop
-            if IsControlPressed(0, 172) then -- Up Arrow
-                previewOffset = previewOffset + vector3(0.0, 0.02, 0.0)
-            end
-            if IsControlPressed(0, 173) then -- Down Arrow
-                previewOffset = previewOffset - vector3(0.0, 0.02, 0.0)
-            end
-            if IsControlPressed(0, 174) then -- Left Arrow
-                previewOffset = previewOffset - vector3(0.02, 0.0, 0.0)
-            end
-            if IsControlPressed(0, 175) then -- Right Arrow
-                previewOffset = previewOffset + vector3(0.02, 0.0, 0.0)
-            end
-            if IsControlPressed(0, 44) then -- Q Key (Rotate Left)
-                previewRotation = previewRotation - 1.0
-            end
-            if IsControlPressed(0, 51) then -- E Key (Rotate Right)
-                previewRotation = previewRotation + 1.0
-            end
-        end
-        Wait(0)
-    end
-end)
 
 RegisterCommand("pickup_prop", function()
     pickupProp()
